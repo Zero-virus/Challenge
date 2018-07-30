3d3d6f015a0933135bbdb51bb6704d6e037746a3
Zero-virus <cristiancastillo.rodriguez@gmail.com>
2018-07-30 17:32:16 -0400

    update

--- challenge.go
+++ challenge.go
@@ -14,44 +14,73 @@ type Test struct {
 	Name string `json:"name"`
 }
 
-func gener(te []Test) <-chan Test {
+/*func gener(te []Test) <-chan Test {
 	out := make(chan []Test)
 	go func() {
 		for _, n := range te {
-			out <- n
+			out <- te
 		}
 		close(out)
 	}()
+	fmt.Println("llamar return")
 	return out
+}*/
+
+func sendjson(c chan Test, w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	v := <-c
+	json.NewEncoder(w).Encode(v)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 
-	w.Header().Set("Content-Type", "application/json")
+	//	var t Test
+	//	json.NewDecoder(r.Body).Decode(&t)
 
-	var t Test
-	json.NewDecoder(r.Body).Decode(&t)
+	var c = make(chan Test)
 
-	var c = make(chan *Test)
-
-	t := &Test{
-		ID:   "daad",
-		Name: "bab",
+	t := []Test{
+		{
+			ID:   "daad",
+			Name: "bab",
+		},
+		{
+			ID:   "daa",
+			Name: "bb",
+		},
 	}
 
+	count := 0
 	go func() {
-		c <- t
+		for _, n := range t {
+			count++
+			c <- n
+
+		}
 		close(c)
 	}()
-	for n := range c {
-		fmt.Println(w, string(n))
+	fmt.Println(len(c))
+
+	for d := 0; d < count; d++ {
+		go sendjson(c, w, r)
 	}
+	/*
+		go func() {
+			c <- t
+			close(c)
+		}()
+
+		}*/
+
 }
+
+func receive()
 
 func main() {
 
 	router := mux.NewRouter()
 
+	//	c := gener()
 	fmt.Println("cerrado")
 
 	router.HandleFunc("/", handler).Methods("GET")
